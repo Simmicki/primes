801c41088c5de111c409d5d479897ea3e83f0e2c
Michael Simonsen <85156514+Simmicki@users.noreply.github.com>
2021-11-11 09:52:40 +0100

    Create main.cpp

---
+++ cpp/main.cpp
@@ -0,0 +1,92 @@
+// Copyright © 2021 Michael Simonsen
+// Programnavn: Primes
+
+#include <iostream>
+#include <iomanip>
+#include <fstream>
+#include <sstream>
+#include <csignal>
+
+using namespace std;
+
+unsigned long long isPrimeNumber(unsigned long long);
+void signalHandler(int);
+
+unsigned long long n = 2;
+
+int main()
+{
+   
+   bool isPrime;
+   unsigned long long startnum;
+   string line;
+   
+   // register signal SIGINT and signal handler  
+   signal(SIGINT, signalHandler);
+   
+   // open "prime.ini" if it exists
+   ifstream myfile;
+   myfile.open("prime.ini");
+
+   if(myfile) {
+      cout << "config file exists." << endl;
+      cout << "reading: prime.ini: ";
+      while ( getline (myfile,line) )
+    {
+      cout << line << '\n' << '\n';
+      stringstream ss;  
+      ss << line;  
+      ss >> startnum;
+    }
+    myfile.close();
+    cout << "done" << endl << endl;
+    
+   } else {
+      cout << "prime.ini doesn't exist" << endl;
+      cout << "creating it now" << endl;
+      ofstream myfile;
+      myfile.open("prime.ini");
+      myfile << "2" << endl;
+      myfile.close();
+      cout << "done" << endl << endl;
+      startnum = 2;
+   }
+   
+   for(n = startnum; n < 18446744073709551615u; n++) {
+      // isPrime will be true for prime numbers
+      isPrime = isPrimeNumber(n);
+
+      if(isPrime == true){
+         cout<<left<<setw(24)<<n;
+         
+	  }
+   }
+   return 0;
+}
+
+// Function that checks whether n is prime or not
+unsigned long long isPrimeNumber(unsigned long long n) {
+   bool isPrime = true;
+
+   for(int i = 2; i <= n/2; i++) {
+      if (n%i == 0) {
+         isPrime = false;
+         break;
+      }
+   }  
+   return isPrime;
+}
+
+void signalHandler( int signum ) {
+   cout << endl << "Interrupt signal (" << signum << ") received.\n";
+
+   // cleanup and close up stuff here  
+   // terminate program
+   ofstream myfile;
+   myfile.open("prime.ini");
+   myfile << n << endl;
+   myfile.close();
+
+   cout << "File saved, now terminating !";
+   exit(signum);  
+}
